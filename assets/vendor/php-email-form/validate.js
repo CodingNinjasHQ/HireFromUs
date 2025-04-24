fe7adf0de399c870ddee044fcdfbfb3f548ec865
vedantsrivastavacn <vedant.srivastava@codingninjas.com>
2025-04-24 15:23:41 +0530

    NEW UI Original for comparison

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -50,35 +50,29 @@
   });
 
   function php_email_form_submit(thisForm, action, formData) {
-    thisForm.querySelector('.loading').classList.add('d-block');
-    thisForm.querySelector('.error-message').classList.remove('d-block');
-    thisForm.querySelector('.sent-message').classList.remove('d-block');
-  
     fetch(action, {
       method: 'POST',
       body: formData,
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
+      headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
-      if (response.ok) {
-        return response.json();
+      if( response.ok ) {
+        return response.text();
       } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
       }
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.message === 'Response Submitted Successfully') {
+      if (data.trim() == 'OK') {
         thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset();
+        thisForm.reset(); 
       } else {
-        throw new Error(data.message || 'Form submission failed');
+        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
       }
     })
     .catch((error) => {
-      displayError(thisForm, error.message || error);
+      displayError(thisForm, error);
     });
   }
 
